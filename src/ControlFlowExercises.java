055042a3be4318e3342118525ee263f24fdc2f04
Ryan Hampton <hamptonryan88@gmail.com>
2023-10-02 10:33:56 -0500

    updates from exercise review

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,102 @@
+import java.math.*;
+import java.util.Scanner;
+public class ControlFlowExercises {
+    public static void main (String[] args) {
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//
+//        int i = 0;
+//
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+//
+//        int i = 100;
+//
+//       while (i >= -10) {
+//           System.out.println(i);
+//           i -= 5;
+//       }
+//
+//        int i = 2;
+//
+//        do {
+//            System.out.println(i);
+//
+//        } while (i <= 1000000);
+//
+//    }
+
+//        long numberTwo = 1 + 1;
+//       do {
+//           System.out.println(numberTwo);
+//           numberTwo *= numberTwo;
+//       } while (numberTwo < 1000000);
+//    }
+//
+//        for (long i = 2; i < 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+
+
+//        for (int i = 0; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Input your fav number: ");
+//        int userInput = scanner.nextInt();
+//
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//
+//        for (int i = 0; i <= userInput; i++) {
+//            int square = i * i;
+//            int cube = i * i * i;
+//            System.out.printf("%d      |   %d     |   %d \n", i, square, cube);
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter a grade: ");
+//        int userInput = scanner.nextInt();
+//
+//        String grade = "";
+//
+//        if (userInput >= 88 && userInput <= 100) {
+//            grade = "A";
+//            if (userInput >= 99) {
+//                grade = "A+";
+//            }
+//        } else if (userInput >= 80 && userInput <= 87) {
+//            grade = "B";
+//            if (userInput >= 86) {
+//                grade = "B+";
+//            }
+//        } else if (userInput >= 67 && userInput <= 79) {
+//            grade = "C";
+//            if (userInput >= 78) {
+//                grade = "C+";
+//            }
+//        } else if (userInput >= 60 && userInput <= 66) {
+//            grade = "D";
+//        } else if (userInput >= 0 && userInput <= 59) {
+//            grade = "F";
+//        }
+//
+//        System.out.println("Grade is: " + grade);
+
+    }
+}
